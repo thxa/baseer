3ecc3b0731c64e344fcd87d13bfd73eddbcb567e
Thamer Al-harbi <33045743+thxa@users.noreply.github.com>
2025-09-03 19:33:05 +0300

    Clean

--- modules/binhead/bx_binhead.c
+++ modules/binhead/bx_binhead.c
@@ -1,39 +1,9 @@
 /**
  * @file bx_binhead.c
- * @brief function that handle magic number of header file.
+ * @brief Handle the magic number of a file header and trigger extensions.
  */
 
 #include "bx_binhead.h"
-
-int reverse_4bytes(int n) {
-    return ((n & 0xFF) << (BYTE * 3) )     |
-        ((n & (0xFF << BYTE*3)) >> BYTE*3) |
-        ((n & (0xFF << BYTE*1)) << BYTE*1) | 
-        ((n & (0xFF << BYTE*2)) >> BYTE*1);
-}
-
-unsigned long long int reverse_8byte_bin(unsigned long long int n) {
-    return
-        ((n & (0xFFULL << (BYTE * 0))) << (BYTE * 7)) |
-        ((n & (0xFFULL << (BYTE * 1))) << (BYTE * 5)) |
-        ((n & (0xFFULL << (BYTE * 2))) << (BYTE * 3)) |
-        ((n & (0xFFULL << (BYTE * 3))) << (BYTE * 1)) |
-        ((n & (0xFFULL << (BYTE * 4))) >> (BYTE * 1)) |
-        ((n & (0xFFULL << (BYTE * 5))) >> (BYTE * 3)) |
-        ((n & (0xFFULL << (BYTE * 6))) >> (BYTE * 5)) |
-        ((n & (0xFFULL << (BYTE * 7))) >> (BYTE * 7));
-}
-
-unsigned long long int reverse_bin(unsigned long long int n) {
-    unsigned long long int x = 0;
-    while(n > 0) {
-        x<<=1;
-        x |= (n & 1);
-        n>>=1;
-    }
-    return x;
-}
-
 unsigned int count_bits(unsigned long long int n) {
     unsigned int cnt = 0;
     while (n) {
@@ -64,16 +34,6 @@ unsigned long long int reverse_bytes(unsigned long long int n) {
     return result;
 }
 
-bool check_4bytes(unsigned int magic_value, unsigned int magic_number) {
-    return magic_value == magic_number || magic_value == reverse_4bytes(magic_number);
-}
-
-bool check(unsigned long long int magic_value, unsigned long long int magic_number) {
-    unsigned long long int clean_bytes = 0xFFULL << (BYTE * 7);
-    return magic_value == magic_number || magic_value == reverse_bytes(magic_number);
-}
-
-
 bool bx_binhead(baseer_target_t *target, unsigned int index, void *arg)
 {   
     if (target == NULL || target->block == NULL || index >= target->partition_count)
@@ -88,6 +48,8 @@ bool bx_binhead(baseer_target_t *target, unsigned int index, void *arg)
         {"ELF", ELF_MAGIC, reverse_bytes(ELF_MAGIC)},
         {"PDF", PDF_MAGIC, reverse_bytes(PDF_MAGIC)},
         {"PNG", PNG_MAGIC, reverse_bytes(PNG_MAGIC)},
+        {"ZIP", ZIP_MAGIC, reverse_bytes(ZIP_MAGIC)},
+        { NULL, 0,         0,                 NULL }
     };
 
     for(int i=0; i< sizeof(magics)/ sizeof(magics[0]); i++)
@@ -107,10 +69,10 @@ bool bx_binhead(baseer_target_t *target, unsigned int index, void *arg)
             }
             p++, mgn++, mgn_r++;
         }
-        printf("\n");
 
         if(flag) {
-            printf("%s\n", magics[i].name);
+            printf("This file is %s\n", magics[i].name);
+            
             break;
         }
 
@@ -119,28 +81,5 @@ bool bx_binhead(baseer_target_t *target, unsigned int index, void *arg)
     }
     free(bp);
 
-
-
-    // printf("%d\n", bp ->source.mem.size);
-
-    // TODO: handle most of magic numbers.
-    // unsigned int magic_value_4byte = *((unsigned int*)block);
-    // if(check_4bytes(magic_value_4byte, ELF_MAGIC)){
-    //     printf("This is ELF file\n");
-    //     return true;
-    // }
-
-    // if(check(magic_value_4byte, PDF_MAGIC)){
-    //     printf("This is PDF file\n");
-    //     return true;
-    // }
-
-
-
-    // unsigned long long magic_value = *((unsigned long long*)block);
-    // if(check(magic_value, PNG_MAGIC)){
-    //     printf("This is PNG file\n");
-    //     return true;
-    // }
     return true;
 }
--- modules/binhead/bx_binhead.h
+++ modules/binhead/bx_binhead.h
@@ -12,8 +12,8 @@
 #include "../bparser/bparser.h"
 
 #define ELF_MAGIC 0x7F454C46 // https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
-#define PNG_MAGIC 0x89504e470d0a1a0a // https://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
-// #define PDF_MAGIC 0x255044462D
+#define PNG_MAGIC 0x89504e470d0a1a0a // https://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html 
+#define ZIP_MAGIC 0x504B
 #define PDF_MAGIC 0x25504446
 #define BYTE 8
 
